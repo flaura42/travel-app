18f727fe4398267de8381fe9f81374953ba1882f
Laura <flaura42@gmail.com>
2020-07-21 00:42:51 -0700

    add (emptyish) style files

---
+++ src/client/index.js
@@ -0,0 +1,14 @@
+import { handleSubmit } from './js/formHandler'
+import { addCountries } from './js/addCountries'
+
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/results.scss'
+
+
+export {
+  handleSubmit,
+  addCountries
+}
